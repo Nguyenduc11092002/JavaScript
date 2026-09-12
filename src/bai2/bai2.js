914b79305d225400515f110dc09be438c98deba6
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-12 18:01:22 +0700

    Upload more for bai2.js and create new folder Bai3

---
+++ src/bai2/bai2.js
@@ -0,0 +1,32 @@
+//Primitive Data types (Các Kiểu Dữ Liệu Nguyên Thủy)
+//Number
+const myAge = 25;
+const myHeight = 1.75;
+console.log(myAge);
+console.log(myHeight);
+
+//string
+const myName = "Nguyen Van A"; //java
+const myAddress = "Ha Noi";
+const name1 = `erc`;
+const name2 = "Nguyen Van B";
+console.log(myName);
+console.log(myAddress);
+console.log(name1);
+console.log(name2);
+
+//boolean: True/False
+const isStudent = true;
+const hasDriverLicense = false;
+console.log(isStudent);
+console.log(hasDriverLicense);
+
+//undefined
+let myJob;
+console.log(myJob);
+
+//null
+const myCar = null;
+console.log(myCar);
+
+console.log("Bai 2");
