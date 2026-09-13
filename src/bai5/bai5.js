24ecf2055b4dbd507275f6d6f418c693e3d24b7d
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-13 17:36:56 +0700

    Upload more for folder Bai5 and create new foder Bai6

--- src/bai5/bai5.js
+++ src/bai5/bai5.js
@@ -1,1 +1,6 @@
+const a = 5;
+const b = "5";
+console.log(a == b); //true vì == chỉ so sánh giá trị, không so sánh kiểu dữ liệu
+console.log(a === b); //false vì === so sánh cả giá trị và kiểu dữ liệu, a là number còn b là string
+
 console.log("This is fifth lecture");
