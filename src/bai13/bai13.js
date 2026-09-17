1f0201cf8f02f10c91bb19322fa95e9403cbd664
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-17 21:26:55 +0700

    Upload folder bai13

---
+++ src/bai13/bai13.js
@@ -0,0 +1,23 @@
+console.log("This is thirteenth lecture");
+//This is normal function
+// function sum(a, b) {
+//   return a + b;
+// }
+
+//This is arrow function lecture
+//Arrow function giúp code ngắn hơn, không khác gì với function thường
+(a, b) => {
+  return a + b;
+};
+
+//This is fully arrow function
+const sum = (a, b) => {
+  return a + b;
+};
+
+console.log(sum(1, 2));
+
+//Một function viết xong tự chạy
+// (function () {
+//   console.log("Say hi");
+// })();
