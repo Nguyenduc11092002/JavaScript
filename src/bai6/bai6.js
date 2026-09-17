5da40852a517a8a2f1b93c68795315b7e7053b81
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-17 18:01:13 +0700

    UPload more for folder bai6

--- src/bai6/bai6.js
+++ src/bai6/bai6.js
@@ -2,3 +2,12 @@ console.log("This is sixth lecture");
 console.log("info"); //In giá trị ra màn hình console
 console.error("oops my mistake"); //Hiển thị lỗi
 console.warn("test warning"); //Hiển thị cảnh báo
+
+let name = "Nguyen Van A";
+let age = 20;
+
+console.log(name, age);
+console.log("Name: " + name + ", Age: " + age);
+console.log("Name:", name, ", Age:", age);
+
+console.log("%cWarning!!", "color: red; font-weight: bold;");
