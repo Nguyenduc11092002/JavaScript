f8274807dae6da8ee6ede3004a052c863bb53cbc
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-17 17:50:52 +0700

    Upload more for folder bai6

--- src/bai6/bai6.js
+++ src/bai6/bai6.js
@@ -1,4 +1,4 @@
 console.log("This is sixth lecture");
-console.log("info");
-console.error("oops my mistake");
-console.warn("test warning");
+console.log("info"); //In giá trị ra màn hình console
+console.error("oops my mistake"); //Hiển thị lỗi
+console.warn("test warning"); //Hiển thị cảnh báo
