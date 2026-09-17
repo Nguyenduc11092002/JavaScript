ec7683b1eced249055a0503351ad2e6534edf415
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-17 21:09:34 +0700

    Upload more for folder bai12

---
+++ src/bai12/bai12.js
@@ -0,0 +1,17 @@
+console.log("This is twenth lecture");
+
+//Function lecture
+function greeting() {
+  console.log("Say hello with everyone");
+}
+
+greeting();
+
+//Calculator of sum of 2 numbers
+function sum(a, b) {
+  //tham số (params)
+  return a + b;
+}
+
+console.log("Sum: ", sum(1, 2)); //đầu vào (input)
+console.log("Cộng chuỗi: ", sum("Nguyen ", "Van A")); // cộng chuỗi
