2cf2f9a6a31a1c1eef0b1817bdced679f4e3555c
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-18 19:14:15 +0700

    Upload more for folder bai16

--- src/bai16/bai16.js
+++ src/bai16/bai16.js
@@ -1,3 +1,8 @@
 console.log("This is sixteenth lecture");
 
 //Đây là bài học về mảng (Array)
+const names = ["Nguyen Van A", "Nguyen Van B", "Tran Van C"];
+const others = ["Nguyen Van A", 25, [1, 2, 3]];
+
+console.log(names);
+console.log(others);
