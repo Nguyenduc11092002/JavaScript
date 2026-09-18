5523d80922022a3a2321072427e4099b6e92b8f2
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-19 06:16:08 +0700

    Upload more for folder

--- src/bai18/bai18.js
+++ src/bai18/bai18.js
@@ -1,1 +1,21 @@
 console.log("This is eighteenth lecture");
+
+//Đây là bài học về duyệt mảng
+const myClass = [
+  "Nguyen Van A",
+  "Nguyen Van B",
+  "Tran Thi C",
+  "Ly Thi D",
+  "Bui Thi E",
+  "Pham Van F",
+  "Le Thi G",
+  "Ho Thi H",
+];
+
+//length là chiều dài của mảng
+console.log(myClass, myClass.length);
+
+//Duyệt mảng bằng for loop
+for (let i = 0; i < myClass.length; i++) {
+  console.log("i = ", i, "and value = ", myClass[i]);
+}
