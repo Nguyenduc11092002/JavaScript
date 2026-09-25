1f3a8aa25b2e7b93fb85fcc39b64acd6f74b1694
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-25 10:44:42 +0700

    Upload more for folder Bai18

--- src/bai18/bai18.js
+++ src/bai18/bai18.js
@@ -19,3 +19,17 @@ console.log(myClass, myClass.length);
 for (let i = 0; i < myClass.length; i++) {
   console.log("i = ", i, "and value = ", myClass[i]);
 }
+
+//Mảng khác bắt đầu từ mảng i+1
+console.log("=====================");
+console.log("Đây là mảng khác với mảng bên trên");
+for (let i = 1; i <= myClass.length; i++) {
+  console.log(" i = ", i, "and value = ", myClass[i - 1]);
+}
+
+//for - each loop : Vòng lặp hiện đơn hơn, dùng để thao tác với đói tượng
+console.log("===================");
+console.log("This is for-each loop");
+myClass.forEach(function (value, index) {
+  console.log("Value = ", value, "Index = ", index);
+});
