0e4387ed66ddc2bf2ce48586e3a37f3b24e9e8dc
Nguyenduc11092002 <DucNMGCS200781@fpt.edu.vn>
2026-09-25 11:52:30 +0700

    Upload more for folder Bai19

--- src/bai19/bai19.js
+++ src/bai19/bai19.js
@@ -1,1 +1,21 @@
 console.log("This is nineteenth lecture");
+
+//For-each này không hỗ trợ việc hưngts giá trị
+const scores = [10, 8, 7, 6, 4, 5];
+scores.forEach((value, index) => {
+  console.log("Value = ", value, " and Index = ", index);
+});
+
+//Map lecture
+console.log("===========");
+console.log("This is map lecture");
+
+//Bắt buộc phải ứng với giá trị trả ra khỏi map
+/*Map dùng để duyệt từng phần tử trong mảng để tạo ra một mảng mới mà
+mỗi phân tử là kết quả biến đổi của phần tử tương ứng trong mảng đầu*/
+const scores2 = scores.map((value, index) => {
+  //   console.log("Value = ", value, " and Index = ", index);
+  return value * 2;
+});
+console.log("scores = ", scores);
+console.log("scores2 = ", scores2);
